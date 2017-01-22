cca29cdb769729ec38fdcd1db5eb3c87a0b0375c
nofranks <olly.edgell@gmail.com>
2017-01-22 19:52:27 +0000

    hello world

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var welcomeButton: UIButton!
+    @IBOutlet weak var myTitle: UIImageView!
+
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showWelcome(_ sender: AnyObject) {
+        myTitle.isHidden = false;
+        background.isHidden = false;
+        welcomeButton.isHidden = true;
+        
+    }
 
 }
 
